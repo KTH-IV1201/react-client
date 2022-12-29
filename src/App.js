a4f00b7c608519f93b76878ca03a9533857e48df
Leif <leifl@kth.se>
2022-12-29 13:15:54 +0100

    adapted from origin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from 'react';
 
 function App() {
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8080/cors', {mode: 'cors'});
+      const data = await response.json();
+      console.log({data});
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    makeAPICall();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Called server on other domain</h1>
     </div>
   );
 }
