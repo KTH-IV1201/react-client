3967ee37b2f87451f82e4e3b4934186e518497b2
Leif <leifl@kth.se>
2023-01-03 13:34:00 +0100

    using heroku url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useEffect} from 'react';
 function App() {
   const makeAPICall = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:8080/cors', {mode: 'cors'});
+      const response = await fetch('https://iv1201-cors-backend.herokuapp.com/cors', {mode: 'cors'});
       const data = await response.json();
       console.log({data});
     } catch (e) {
